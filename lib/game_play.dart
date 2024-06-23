6cd436327645ce4335516aba2fabeb0d7a42f470
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2024-06-23 19:47:51 -0300

    update pre deploy

--- lib/game_play.dart
+++ lib/game_play.dart
@@ -5,7 +5,6 @@ class GameEstado extends StatelessWidget {
   const GameEstado({super.key, required this.titulo});
   final String titulo;
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,40 +26,38 @@ class XyloPhone extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        XyloKey(),
+        XyloKey(1, Colors.red),
+        XyloKey(2, Colors.orange),
+        XyloKey(3, Colors.yellow),
+        XyloKey(4, Colors.lime),
+        XyloKey(5, Colors.green),
+        XyloKey(6, Colors.cyan),
+        XyloKey(7, Colors.blue),
       ],
     );
   }
 }
 
-class XyloKey extends StatefulWidget {
-  const XyloKey({super.key});
+class XyloKey extends StatelessWidget {
+  const XyloKey(this.nota, this.color, {super.key});
+
+final int nota;
+final Color color;
 
   @override
-  State<StatefulWidget> createState() => _XyloKey();
-
-}
-
-class _XyloKey extends State<XyloKey>{
-
-  @override
-  void initState(){
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return TextButton(
       onPressed: () {
+        //AudioCache.instance = AudioCache(prefix: '');
         final player = AudioPlayer();
-        player.play(AssetSource('sounds/note1.wav'));
+        player.play(AssetSource('audio/note$nota.wav'));
       },
       child: SizedBox(
         width: 250.0,
-        height: 60.0,
+        height: 57.0,
         child: DecoratedBox(
           decoration: BoxDecoration(
-            color: Colors.red,
+            color: color,
             borderRadius: BorderRadius.circular(12),
           ),
           child: Row(
@@ -69,23 +66,19 @@ class _XyloKey extends State<XyloKey>{
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
-                  width: 10,
-                  height: 10,
+                  width: 20,
+                  height: 20,
                   decoration: const BoxDecoration(
-                      color: Colors.grey,
-                      shape: BoxShape.circle
-                  ),
+                      color: Colors.grey, shape: BoxShape.circle),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
-                  width: 10,
-                  height: 10,
+                  width: 20,
+                  height: 20,
                   decoration: const BoxDecoration(
-                      color: Colors.grey,
-                      shape: BoxShape.circle
-                  ),
+                      color: Colors.grey, shape: BoxShape.circle),
                 ),
               )
             ],
@@ -94,6 +87,4 @@ class _XyloKey extends State<XyloKey>{
       ),
     );
   }
-
 }
-
